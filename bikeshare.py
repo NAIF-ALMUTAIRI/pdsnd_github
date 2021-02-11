1bf76abef7b04f7ea40e4f40a29ad3d894423950
NAIF <n.m.1416y@gmail.com>
2021-02-11 22:09:00 +0300

    i add in line 20 welcome message

--- bikeshare.py
+++ bikeshare.py
@@ -17,6 +17,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
+    print("Welcome to bikeshare project\n")
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     for city in CITY_DATA:
         city = input('choose city please: chicago, new york city, washington:  ').lower()
